e7431ffd129d7cc93efd3dbce563a8a98b217638
Han Seong Ju <gkstjdwntl@gmail.com>
2026-05-01 15:28:36 +0900

    Textbook

---
+++ Textbook/src/Ch01/seco6/Hello.java
@@ -0,0 +1,7 @@
+package Ch01.seco6;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+}
---
+++ Textbook/src/Ch02/sec02/IntergerLiteralExample.java
@@ -0,0 +1,35 @@
+package Ch02.sec02;
+
+public class IntergerLiteralExample {
+	public static void main(String[] args) {
+		int var1 = 0b1011;
+		int var2 = 0206;
+		int var3 = 365;
+		int var4 = 0xb3;
+		
+		System.out.println("var1: " + var1);
+		System.out.println("var2: " + var2);
+		System.out.println("var3: " + var3);
+		System.out.println("var4: " + var4);
+		
+		byte var5 = -128;
+		byte var6 = -30;
+		byte var7 = 0;
+		byte var8 = 30;
+		byte var9 = 127;
+		
+		System.out.println(var5);
+		System.out.println(var6);
+		System.out.println(var7);
+		System.out.println(var8);
+		System.out.println(var9);
+		
+		long var10 = 10;
+		long var11 = 20L;
+		long var12 = 1000000000L;
+		
+		System.out.println(var10);
+		System.out.println(var11);
+		System.out.println(var12);
+	}
+}
