264de5df57c0332a1a2641a6ad7d9c256c28a1db
Han Seong Ju <gkstjdwntl@gmail.com>
2026-05-01 15:27:24 +0900

    CodingTest

---
+++ CodingTest/src/sub1/Solution.java
@@ -0,0 +1,18 @@
+package sub1;
+
+import java.util.Scanner;
+
+/*
+ *  날짜 : 2026/05/01
+ *  이름 : 한성주
+ *  내용 : 프로그래머스 기출문제 1번 문자출력
+ */
+
+public class Solution {
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+	}
+}
+
